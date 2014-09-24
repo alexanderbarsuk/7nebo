d98f411cfeee4bdd573bb07a88f215358dfb2882
Alexander Barsuk <barssukaleksandr@gmail.com>
2014-09-24 22:23:47 +0300

    new base application

--- www/index.php
+++ www/index.php
@@ -1,8 +1,19 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
  */
+chdir(dirname(__DIR__));
 
+// Setup autoloading
+//require 'init_autoloader.php';
+
+include  'D:/OpenServer/userdata/ZendFramework/library/Zend/Loader/AutoloaderFactory.php';
+Zend\Loader\AutoloaderFactory::factory(array(
+    'Zend\Loader\StandardAutoloader' => array(
+        'autoregister_zf' => true
+    )
+));
+
+// Run the application!
+Zend\Mvc\Application::init(require 'private/config/application.config.php')->run();
